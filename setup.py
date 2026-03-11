8fc1a19edc35932cdb32064d906c3abd28adedbd
Ashik Hasan Redoy <ashikhasanhredoy@gmail.com>
2026-03-11 22:15:19 +0600

    did all pipeline and done the project

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements(file_path: str) -> List[str]:
 setup(
     name="ml_project",
     version="0.1",
-    author="Ashik Hasan",
+    author="Ashik Hasan Redoy",
     author_email="ashikhasanhredoy@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements("requirements.txt")
